2a70d3a24a9008ccf606929af09fa11e737949ed
Okinz <steven14812@gmail.com>
2021-02-11 17:45:53 -0500

    Added post routes for register and login

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,6 +10,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+  email: String,
+  password: String,
+}
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
   res.render("home");
@@ -23,6 +32,38 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err){
+    if(err){
+      console.log(err);
+    } else {
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }
+  })
+});
+
+app.post("/register", function(req, res){
+
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
 
 
 
